f4b1075171dd36fd9a01d6d3eee87d1e62027665
-g <-g>
2023-06-28 13:06:51 -0300

    create contactos crud

---
+++ src/routes/contactos/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Table from "../Table.svelte";
+
+    let header = [ "id", "E-Mail", "Nombre", "Apellidoasl dfja;sdlf jadlfajsld ;jasdflkjads ;fakdsjfaldsf kja ;lsdkfj", "Ubicación" , "E-Mail", "Nombre", "Apellidoasl dfja;sdlf jadlfajsld ;jasdflkjads ;fakdsjfaldsf kja ;lsdkfj", "Ubicación" ];
+
+</script>
+<h1 class="text-xl">Contactos</h1>
+
+<Table
+    header = {header}
+    data = {header}
+    />
