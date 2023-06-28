0fd193705ebc905eec5cd6a95bc66376772d66c7
-g <-g>
2023-06-28 13:06:16 -0300

    create resizable panel  component

---
+++ src/routes/ResizablePanel.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Draggable from "./Draggable.svelte";
+
+    let className = "", rightBorderStyle = "", leftBorderStyle = "";
+
+    export {className as class, rightBorderStyle, leftBorderStyle}
+</script>
+
+<style>
+</style>
+
+
+<div class="flex flex-row  {className}"  >
+    <Draggable class="w-2 hidden" style="{leftBorderStyle}"></Draggable>
+    <div class="mx-1 " >
+        <slot />
+    </div>
+    <Draggable class="w-2 cursor-ew-resize" style="{rightBorderStyle}"></Draggable>
+</div>
