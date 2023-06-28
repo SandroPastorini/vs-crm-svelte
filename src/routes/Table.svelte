90297be36b45ca727da7c85ef950166c9b1bfa2f
-g <-g>
2023-06-28 13:06:31 -0300

    create table component

---
+++ src/routes/Table.svelte
@@ -0,0 +1,29 @@
+<script>
+	import ResizablePanel from "./ResizablePanel.svelte";
+
+    export let header = [];
+    export let data;
+</script>
+
+<div>
+    <table class="overflow-scroll w-max select-none ">
+        <thead>
+            {#each header as item,i}
+            <th class="text-left border-slate-100 border-2">
+                <ResizablePanel class="">
+                    {item}
+                </ResizablePanel>
+            </th>
+            {/each}
+        </thead>
+        <tbody>
+            <tr>
+                {#each data as item,i}
+                <td>
+                    {item}
+                </td>
+                {/each}
+            </tr>
+        </tbody>
+    </table>
+</div>
